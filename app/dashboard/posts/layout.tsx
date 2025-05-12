6c9be83cfeb6718148b526802885d6638b998f9d
Héctor leal <flamithas@comunidad.unam.mx>
2025-05-11 19:44:03 -0600

    style: update new visual style

--- app/dashboard/posts/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex flex-col w-full h-full">
-      <div className="flex-1">{children}</div>
-    </div>
-  );
-}
